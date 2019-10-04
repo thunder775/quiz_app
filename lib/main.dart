9b02fc3205d8a0678edd18ccf3a6a7f4c554e121
rahul masih <rahulmasih61@gmail.com>
2019-10-04 14:17:27 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        body: QuizApp(),
-      ),
+      home: QuizApp(),
     ),
   );
 }
@@ -18,8 +16,41 @@ class QuizApp extends StatefulWidget {
 class _QuizAppState extends State<QuizApp> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Text("Your content here"),
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.question_answer),
+        title: Text('Quiz App'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+              flex: 6,
+              child: Center(child: Text('questions come in this section'))),
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.only(left:40.0,right: 40,top: 32,bottom: 10),
+              child: RaisedButton(
+                child: Text('Correct'),
+                color: Colors.green,
+                onPressed: () {},
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.only(left:40.0,right: 40,top: 10,bottom: 32),
+              child: RaisedButton(
+                child: Text('Incorrect'),
+                color: Colors.red,
+                onPressed: () {},
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
